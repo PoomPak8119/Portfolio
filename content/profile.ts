9f07a0103c318ead70f96776f56bd0ecdda713ef
Passapol Phukhang <passapolpoom@gmail.com>
2026-09-24 08:53:32 +0100

    fix: update public CV link

--- content/profile.ts
+++ content/profile.ts
@@ -13,7 +13,7 @@ export const profile = {
   email: "passapolpoom@gmail.com",
   description:
     "I bridge business, people, and emerging technology to turn organisational challenges into practical digital and AI solutions.",
-  cv: "https://drive.google.com/file/d/1xp206xIiqt1ri4jYeMh-0trrPnlDNrVh/view?usp=sharing" as
+  cv: "https://drive.google.com/file/d/1dRPOxoPIMDDp-9Oqy9hqCFDStpEGEEU8/view?usp=sharing" as
     | string
     | null,
   socialLinks: [
